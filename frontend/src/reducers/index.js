27ae6c17f572a7e46336736d3c8b11a5d8e8544f
alexanderasche <alexander.asche@gmail.com>
2018-05-01 15:06:07 -0400

    gh-page

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import sample_cocktails from './sample_cocktails';
-import user_reducer from './user_reducer';
+// import user_reducer from './user_reducer';
 import app_reducer from './app_reducer';
 
 const rootReducer = combineReducers({
   app: app_reducer,
   cocktails: sample_cocktails,
-  user: user_reducer
+  // user: user_reducer
 });
 
 export default rootReducer;
